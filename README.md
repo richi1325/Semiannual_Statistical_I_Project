a82011f5b231835d0699a3f05946f41696268be2
Jose Ricardo Mendoza Villar <richi_1325@hotmail.com>
2021-01-17 13:50:26 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# This repository is for the control version of the project of statistics I course  
+Created by:  
+* Gómez González Astrid Yoatziry.  
+* Mendoza Villar Jose Ricardo.  
+* Navarro Ramos Karen Lizbeth.
---
+++ file.R
@@ -0,0 +1,8 @@
+library(readr)
+bestsellers <- read_csv("Estadística/bestsellers.csv")
+z<-!is.na(bestsellers$Reviews)&!is.na(bestsellers$`Price(USD)`)&!is.na(bestsellers$Type)
+x<-lapply(bestsellers, function(x) x[z])
+df <- data.frame(Reviews = x$Reviews,
+                 `Price(USD)` = x$`Price(USD)`,
+                 Type = x$Type)
+write.csv(df,'Estadística/bestsellers_without_naValues.csv')
